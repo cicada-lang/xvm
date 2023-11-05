93b6ed2aaa5011d595e6bd0446386ae954c0468a
Xie Yuheng <xyheme@gmail.com>
2023-11-05 18:41:16 +0800

    up

--- src/instruction.h
+++ src/instruction.h
@@ -1,11 +1,9 @@
 #pragma once
 
-// #define END ((instruction_t) 0)
-#define CALL ((instruction_t) 1)
-#define LITERAL_INT ((instruction_t) 2)
-#define LITERAL_STRING ((instruction_t) 3)
-#define LITERAL_WORD ((instruction_t) 4)
-// Double-precision floating-point
-#define LITERAL_FLOAT ((instruction_t) 5)
-#define LITERAL_BOOLEAN ((instruction_t) 6)
-#define LITERAL_NULL ((instruction_t) 7)
+#define CALL              ((instruction_t) 0x01)
+#define LITERAL_INT       ((instruction_t) 0x02)
+#define LITERAL_STRING    ((instruction_t) 0x03)
+#define LITERAL_WORD      ((instruction_t) 0x04)
+#define LITERAL_BOOLEAN   ((instruction_t) 0x05)
+#define LITERAL_NULL      ((instruction_t) 0x06)
+#define LITERAL_FLOAT     ((instruction_t) 0x07) // Double-precision floating-point
