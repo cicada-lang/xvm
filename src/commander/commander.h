47ead9a6e1be78c679ac5edcb842616019a9a4a5
Xie Yuheng <xyheme@gmail.com>
2024-11-20 09:04:07 +0800

    ambr commander_define commander_use

--- src/commander/commander.h
+++ src/commander/commander.h
@@ -4,6 +4,6 @@ commander_t *commander_new(const char *name, const char *version, int argc, char
 void commander_destroy(commander_t **self_pointer);
 
 void commander_add(const commander_t *self, command_t *command);
-void commander_define(const commander_t *self, command_plugin_t *plugin);
+void commander_use(const commander_t *self, command_plugin_t *plugin);
 void commander_help(const commander_t *self);
 int commander_run(const commander_t *self);
--- src/x.c
+++ src/x.c
@@ -7,11 +7,11 @@ main(int argc, char *argv[]) {
     commander_t *runner = commander_new(
         "xvm", X_VERSION, argc, argv);
 
-    commander_define(runner, run_command);
-    commander_define(runner, repl_command);
-    commander_define(runner, self_test_command);
-    commander_define(runner, default_help_command);
-    commander_define(runner, default_version_command);
+    commander_use(runner, run_command);
+    commander_use(runner, repl_command);
+    commander_use(runner, self_test_command);
+    commander_use(runner, default_help_command);
+    commander_use(runner, default_version_command);
 
     int status_code = commander_run(runner);
     commander_destroy(&runner);
