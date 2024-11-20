8b3588c630bd7660d088230113cce18d492aa087
Xie Yuheng <xyheme@gmail.com>
2024-11-20 08:54:51 +0800

    ambr commander_mount commander_define

--- src/x.c
+++ src/x.c
@@ -7,11 +7,11 @@ main(int argc, char *argv[]) {
     commander_t *runner = commander_new(
         "xvm", X_VERSION, argc, argv);
 
-    commander_mount(runner, run_command);
-    commander_mount(runner, repl_command);
-    commander_mount(runner, self_test_command);
-    commander_mount(runner, default_help_command);
-    commander_mount(runner, default_version_command);
+    commander_define(runner, run_command);
+    commander_define(runner, repl_command);
+    commander_define(runner, self_test_command);
+    commander_define(runner, default_help_command);
+    commander_define(runner, default_version_command);
 
     int status_code = commander_run(runner);
     commander_destroy(&runner);
