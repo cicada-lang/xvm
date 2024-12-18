5743e6c81b76189bf7024ed19da722b54ce6a500
Xie Yuheng <xyheme@gmail.com>
2024-12-18 13:55:40 +0800

    up

--- TODO.md
+++ TODO.md
@@ -1,8 +1,23 @@
-# emu
+# vm
 
-[emu] setup `emu/`
-[emu] `x emu` command
-[emu] `value_t` -- 用最经典的 4-bit tag + 62-bit value runtime 编码
+
+[vm] setup `vm/`
+[vm] `value_t` -- 用最经典的 4-bit tag + 62-bit value runtime 编码
+
+```
+xobject_t  -- 0x0000
+- xstring_t -- STRING_VALUE
+- xtuple_t  -- TUPLE_VALUE
+xaddr_t    -- 0x0001
+xint_t     -- 0x0010
+
+x_immediate_t -- 0x0011
+- xbool_t    --
+- xchar_t    --
+- xnull_t    --
+```
+
+[vm] `x emu` command
 
 # asm
 
--- src/modules/memory/allocate.c
+++ src/modules/memory/allocate.c
@@ -1,15 +1,19 @@
 #include "index.h"
 
+// The address of a block returned by malloc or realloc in GNU systems
+// is always a multiple of eight (or sixteen on 64-bit systems).
+// -- https://www.gnu.org/software/libc/manual/html_node/Aligned-Memory-Blocks.html
+
 static bool
-is_16_byte_aligned(void *address) {
-    return (((uintptr_t) address) & ((uintptr_t) 0xf)) == 0;
+is_8_bytes_aligned(void *address) {
+    return (((uintptr_t) address) & ((uintptr_t) 0x7)) == 0;
 }
 
 void *
 allocate(size_t size) {
     void *address = calloc(1, size);
     assert(address);
-    assert(is_16_byte_aligned(address));
+    assert(is_8_bytes_aligned(address));
     return address;
 }
 
@@ -17,7 +21,7 @@ void *
 allocate_many(size_t size, size_t unit_size) {
     void *address = calloc(size, unit_size);
     assert(address);
-    assert(is_16_byte_aligned(address));
+    assert(is_8_bytes_aligned(address));
     return address;
 }
 
