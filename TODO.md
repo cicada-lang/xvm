b23631342a19a3b216c7864108017a6d2802b2ae
Xie Yuheng <xyheme@gmail.com>
2024-12-18 15:58:52 +0800

    up

--- TODO.md
+++ TODO.md
@@ -1,25 +1,23 @@
 # vm
 
-[vm] `frame_t`
+[vm] setup `frame`
+[vm] `frame_t` -- only has `program_counter` for now -- will have local variables
+[vm] `frame_new`
+[vm] `frame_destroy`
+
 [vm] `vm_new` -- `frame_stack` -- use `frame_destroy`
+
+[vm] `vm_step`
+[vm] `vm_run_until`
+
+[vm] `execute`
 
 values/xint
 values/xaddress
-values/xobject
+values/xobject -- with `kind`
 
-```
-xobject_t   -- 0
-- xstring_t -- XSTRING
-- xtuple_t  -- XTUPLE
-xaddress_t  -- 1 -- address in code is different from address of pointer
-xint_t      -- 2
-
-x_immediate_t -- 3
-- xbool_t    --
-- xchar_t    --
-- xnull_t    --
-- xop_t      --
-```
+objects/xstring
+objects/xtuple
 
 [vm] `x emu` command
 
--- src/vm/types.h
+++ src/vm/types.h
@@ -5,13 +5,24 @@ typedef void *value_t;
 // value = higher 61 bits data + lower 3 bits tag.
 // thus we have 8 tags.
 
+// address in vm ram is different from
+// address of pointer (object).
 typedef enum {
-    XOBJECT = 0,
-    // address in code is different from
-    // address of pointer (object).
-    XADDRESS = 1,
-    XINT = 2,
-    XIMMEDIATE = 3,
+    XOBJECT       = 0, // 0b000
+    XADDRESS      = 1, // 0b001
+    XINT          = 2, // 0b010
+    // TODO       = 3, // 0b011
+    // TODO       = 4, // 0b100
+    // TODO       = 5, // 0b101
+    // TODO       = 6, // 0b110
+    XEXTENDED     = 7, // 0b111
 } tag_t;
+
+// extended_tag_t ?
+
+// - xbool_t  -- TODO 0b111
+// - xchar_t  -- TODO 0b111
+// - xnull_t  -- TODO 0b111
+// - xop_t    -- TODO 0b111
 
 typedef struct vm_t vm_t;
