66a22fe18bf7d703c93a9a3e88d6f179df05fedd
Xie Yuheng <xyheme@gmail.com>
2024-12-18 15:19:47 +0800

    [vm] ``tag_t` as enum

--- src/vm/types.h
+++ src/vm/types.h
@@ -1,4 +1,15 @@
 #pragma once
 
 typedef void *value_t;
-typedef uint8_t tag_t;
+
+// value = higher 61 bits data + lower 3 bits tag.
+// thus we have 8 tags.
+
+typedef enum {
+    XOBJECT = 0,
+    // address in code is different from
+    // address of pointer (object).
+    XADDRESS = 1,
+    XINT = 2,
+    XIMMEDIATE = 3,
+} tag_t;
