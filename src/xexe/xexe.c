ffce790bad9ca4a799f0f073a2df487414fd7d08
Xie Yuheng <xyheme@gmail.com>
2024-12-21 04:19:54 +0800

    ambr blob_length blob_size

--- src/xexe/xexe.c
+++ src/xexe/xexe.c
@@ -1,1 +1,9 @@
 #include "index.h"
+
+xexe_t *
+xexe_new(size_t size, uint8_t *bytes) {
+    xexe_t *self = new(xexe_t);
+    self->size = size;
+    self->bytes = bytes;
+    return self;
+}
--- src/xexe/xexe.h
+++ src/xexe/xexe.h
@@ -4,3 +4,5 @@ struct xexe_t {
     size_t size;
     uint8_t *bytes;
 };
+
+xexe_t *xexe_new(size_t size, uint8_t *bytes);
