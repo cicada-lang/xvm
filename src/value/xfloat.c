b9b0dfad0930b5709552f4afc76ced4a14a48f63
Xie Yuheng <xyheme@gmail.com>
2024-12-22 06:40:10 +0800

    [value] xfloat_add xfloat_sub xfloat_mul xfloat_div xfloat_mod

--- src/value/xfloat.c
+++ src/value/xfloat.c
@@ -17,3 +17,28 @@ to_double(value_t value) {
     assert(is_xfloat(value));
     return (double) (((uint64_t) value) & (0xffffffffffffff07));
 }
+
+value_t
+xfloat_add(value_t x, value_t y) {
+    return xint(to_int64(x) + to_int64(y));
+}
+
+value_t
+xfloat_sub(value_t x, value_t y) {
+    return xfloat(to_double(x) - to_double(y));
+}
+
+value_t
+xfloat_mul(value_t x, value_t y) {
+    return xfloat(to_double(x) * to_double(y));
+}
+
+value_t
+xfloat_div(value_t x, value_t y) {
+    return xfloat(to_double(x) / to_double(y));
+}
+
+value_t
+xfloat_mod(value_t x, value_t y) {
+    return xfloat(fmod(to_double(x), to_double(y)));
+}
